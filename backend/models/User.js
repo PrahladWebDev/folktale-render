4e9cf42ea18a6ce1097e60dabfd35c9172352983
Prahlad Singh <prahladsingh7512@gmail.com>
2025-06-19 20:42:56 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false }, // New field for admin role
 });
