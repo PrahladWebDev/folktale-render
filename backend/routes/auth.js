921c2209d36f8dfa270c0461fd952c600d163c72
Prahlad Singh <prahladsingh7512@gmail.com>
2025-06-19 22:05:32 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import { auth } from '../middleware/auth.js';
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 // Generate OTP
 const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
