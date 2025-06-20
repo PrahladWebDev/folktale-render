661133a07cd3cc9e392b690b976c2257da9990af
Prahlad Singh <prahladsingh7512@gmail.com>
2025-06-20 17:16:40 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,10 +4,11 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false }, // New field for admin role
+  isAdmin: { type: Boolean, default: false },
   isVerified: { type: Boolean, default: false },
   otp: { type: String },
   otpExpires: { type: Date },
+  profileImageUrl: { type: String }, // New field for profile image URL
 });
 
 export default mongoose.model('User', userSchema);
