e54ca92a6135f009e34063e7682037f868c6e731
Prahlad Singh <prahladsingh7512@gmail.com>
2025-06-25 16:47:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./ThemeContext"; // Import ThemeProvider
 import Home from "./pages/Home";
 import FolktaleDetail from "./pages/FolktaleDetail";
 import Login from "./pages/Login";
@@ -11,25 +12,28 @@ import Navbar from "./components/Navbar";
 import MapFolktaleExplorer from "./components/MapFilter";
 import BookmarkedFolktale from "./pages/BookmarkedFolktale";
 import Profile from './components/Profile';
+import './App.css'; // Import global styles
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<MapFolktaleExplorer />} />
-        <Route path="/bookmarks" element={<BookmarkedFolktale />} />
-        <Route path="/folktale/:id" element={<FolktaleDetail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify-otp" element={<VerifyOTP />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<MapFolktaleExplorer />} />
+          <Route path="/bookmarks" element={<BookmarkedFolktale />} />
+          <Route path="/folktale/:id" element={<FolktaleDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-otp" element={<VerifyOTP />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
