102b29a41f5646a48b60924c7437692f34fa3757
Prahlad Singh <prahladsingh7512@gmail.com>
2025-06-26 11:14:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home";
 import FolktaleDetail from "./pages/FolktaleDetail";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import VerifyOTP from './components/VerifyOTP';
+import EmailVerificationSent from "./pages/EmailVerificationSent";
+import VerifyEmail from "./pages/VerifyEmail";
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import AdminPanel from "./pages/AdminPanel";
@@ -23,7 +24,8 @@ function App() {
         <Route path="/folktale/:id" element={<FolktaleDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/verify-otp" element={<VerifyOTP />} />
+        <Route path="/email-verification-sent" element={<EmailVerificationSent />} />
+        <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/admin" element={<AdminPanel />} />
