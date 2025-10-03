2efdb17ab5da4f6547cddd7afb4536f7eb39554a
Prahlad Singh <prahladsingh7512@gmail.com>
2025-10-03 15:28:48 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import SearchBar from '../components/SearchBar';
 
 function Login() {
   const [email, setEmail] = useState('guest@gmail.com');
-  const [password, setPassword] = useState('guest1234');
+  const [password, setPassword] = useState('Guest1234');
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
